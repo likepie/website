4e6cc814ded971d055e138989d0d4a2411af442b
Simon Dann <simon.dann@gmail.com>
2021-04-14 22:16:20 +0100

    figuring out if netlify is smart with imports

--- netlify/functions/vote.js
+++ netlify/functions/vote.js
@@ -1,5 +1,5 @@
 const querystring = require('querystring');
-import { createClient } from '@supabase/supabase-js'
+const createClient = require('@supabase/supabase-js');
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
