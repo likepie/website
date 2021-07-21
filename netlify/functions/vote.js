8bf548dace16a81877022a2df7286197e8cd37d4
Simon Dann <carbontwelve@users.noreply.github.com>
2021-07-21 13:29:50 +0100

    Testing supabase inserts

--- netlify/functions/vote.js
+++ netlify/functions/vote.js
@@ -4,6 +4,21 @@ const { createClient } = require('@supabase/supabase-js');
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 exports.handler = async function(event, context) {
+    const input = querystring.decode(event.body);
+    const { data, error } = await supabase
+        .from('polls')
+        .insert([
+            { question: 'Hello world', options: input }
+        ]);
+    
+    if (error) {
+        return {
+            statusCode: 200,
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(error)
+        };
+    }
+    
     return {
         statusCode: 200,
         headers: {'Content-Type': 'application/json'},
